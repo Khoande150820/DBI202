852e498de2a2d0484b3cef0ef0ae7651770d8407
Khoande150820 <khoandhe150820@fpt.edu.vn>
2020-11-18 21:51:19 +0700

    dd

--- sqlQuery.sql
+++ sqlQuery.sql
@@ -1,54 +1,49 @@
-create database assignment
-use assignment
-
-CREATE TABLE Team(
-	id INT PRIMARY KEY,
-	name NVARCHAR(50),
-	playerID INT,
-	coachID INT,
-)
 
 
 CREATE TABLE Player(
-	id INT PRIMARY KEY,
-	name NVARCHAR(50),
-	role VARCHAR(5),
-	teamID INT,
-	penalty INT,
-	FOREIGN KEY (teamID) REFERENCES Team(ID)
+	id CHAR(8) PRIMARY KEY NOT NULL CHECK(id LIKE 'KORP[0-9][0-9][0-9][0-9]'),
+	name NVARCHAR(100),
+	gender CHAR(1)  DEFAULT 'M' CHECK(Gender IN('F','M')) NOT NULL,
+	role NVARCHAR(10)
 )
 
 CREATE TABLE Coach(
-	id INT PRIMARY KEY,
-	name nvarchar(50),
-	teamID INT,
-	FOREIGN KEY (teamID) REFERENCES Team(ID)
+	id CHAR(8) PRIMARY KEY NOT NULL CHECK(id LIKE 'KORC[0-9][0-9][0-9][0-9]'),
+	name NVARCHAR,
+	gender CHAR(1)  DEFAULT 'M' CHECK(Gender IN('F','M')) NOT NULL,
+	yearExperience INT,
 )
 
+CREATE TABLE Team(
+	id INT PRIMARY KEY,
+	name NVARCHAR(100),
+	Owner NVARCHAR(100),
+	Status BIT,
+	coachId CHAR(8) UNIQUE NOT NULL
+	FOREIGN KEY (coachId) REFERENCES Coach(id)
+)
 
 CREATE TABLE Season(
 	year INT PRIMARY KEY,
-	teamID int,
-	teamPoint int,
-	MatchID int,
-	FOREIGN KEY (teamID) REFERENCES Team(id)
+	monthBegin INT CHECK (monthBegin BETWEEN 1 AND 12),
+	monthEnd INT CHECK (monthEnd BETWEEN 1 AND 12 AND monthEnd>monthBegin)
 )
 
-CREATE TABLE Match(
-	ID INT PRIMARY KEY,
-	Date date,
-	Team1 int,
-	Team2 int,
-	winner int,
-	Season int,
-	FOREIGN KEY (Season) REFERENCES Season(Year)
+CREATE TABLE Contract(
+	PlayerID CHAR(8),
+	TeamID INT,
+	DateSign DATE,
+	effectiveYear INT,
+	FOREIGN KEY (PlayerID) REFERENCES Player(id),
+	FOREIGN KEY (TeamID) REFERENCES Team(id),
+	PRIMARY KEY (PlayerID, TeamID)
 )
 
-
-CREATE TABLE Penalty(
-	id INT PRIMARY KEY,
-	playerID INT,
-	reason NVARCHAR(100),
-	fine INT,
-	FOREIGN KEY (playerID) REFERENCES Player(ID)
+CREATE TABLE Achievement(
+	TeamID INT,
+	SeasonID INT,
+	Point INT
+	FOREIGN KEY (TeamID) REFERENCES Team(id),
+	FOREIGN KEY (SeasonID) REFERENCES Season(year),
+	PRIMARY KEY (TeamID, SeasonID)
 )
